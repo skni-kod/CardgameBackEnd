37a843f8ca4ff4fcbe0459794727d0cbcf0d225a
myzant123 <38396782+myzant123@users.noreply.github.com>
2021-05-18 21:04:09 +0200

    xd

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
+const PORT = process.env.PORT || 5000;
 const express = require('express');
 
 const app = express();
-
-app.get('/', function (req, res, next) {
-    res.json({
-        'status': 'Sukces'
-    });
-});
 
 module.express = app;
