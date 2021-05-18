ddbcc2977a04960b807e7e57818c9c796f1343d6
myzant123 <38396782+myzant123@users.noreply.github.com>
2021-05-18 20:25:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', function (req, res, next) {
+    res.json({
+        'status': 'Sukces'
+    });
+});
+
 module.express = app;
