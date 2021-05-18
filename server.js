aec01ef655930eefad785a74161cb3219599916b
myzant123 <38396782+myzant123@users.noreply.github.com>
2021-05-18 20:31:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 
+app.get('/', function (req, res, next) {
+    res.json({
+        'status': 'Sukces'
+    });
+});
+
 server.listen(port, () => {
     console.log('Działa Qrwa');
 });
