205cf97511c692aa6ed421a20810ca736cc6156f
IlikeTrains <sewerynpresnal@gmail.com>
2021-06-23 15:58:08 +0200

    CardSet, WhiteCard

--- index.js
+++ index.js
@@ -36,23 +36,18 @@ sequelize.authenticate()
   console.log('Nawiazano polaczenie z baza danych');
 })
 .catch(err => {
-  console.error('Nie udalo sie nawiazac po?aczenia z baza danych');
+  console.error('Nie udalo sie nawiazac polaczenia z baza danych');
 })
 
+const CardSet = sequelize.define('sets', { name: DataTypes.TEXT });
 const BlackCard = sequelize.define('black_cards', { text: DataTypes.TEXT });
+CardSet.hasMany(BlackCard, { as: 'BlackCards'});
+const WhiteCard = sequelize.define('cards_cards', { text: DataTypes.TEXT });
+CardSet.hasMany(WhiteCard, { as: 'WhiteCards'});
 
 sequelize.sync({force: true})
 .then(() => {
   console.log('Utworzono tabele w bazie danych');
-  BlackCard.bulkCreate([
-    { text: 'Ala ma kota' },
-    { text: 'Ala ma psa' },
-    { text: 'czerwona kartka dla Krychowiaka xdd' }
-  ]).then(function(){
-    return BlackCard.findAll();
-  }).then(function(cards){
-    console.log(cards);
-  });
 });
 
 
